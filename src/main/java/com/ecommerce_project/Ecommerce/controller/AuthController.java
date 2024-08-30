1a30fb35be4de230e81cd6db31f8eea4f5e40932
mohamed <mabdelal53@github.com>
2024-08-30 21:41:56 +0300

    Authentiaction Update

--- src/main/java/com/ecommerce_project/Ecommerce/controller/AuthController.java
+++ src/main/java/com/ecommerce_project/Ecommerce/controller/AuthController.java
@@ -1,11 +1,14 @@
 package com.ecommerce_project.Ecommerce.controller;
 
+import com.ecommerce_project.Ecommerce.DTO.AuthResponseDTO;
 import com.ecommerce_project.Ecommerce.DTO.LoginDTO;
 import com.ecommerce_project.Ecommerce.DTO.RegisterationDTO;
 import com.ecommerce_project.Ecommerce.repository.RoleRepo;
 import com.ecommerce_project.Ecommerce.repository.UserRepo;
+import com.ecommerce_project.Ecommerce.security.JWTGenerator;
 import com.ecommerce_project.Ecommerce.service.RegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -26,6 +29,7 @@ public class AuthController {
     private RoleRepo roleRepo;
     private PasswordEncoder passwordEncoder;
     private RegisterService registerService;
+    private JWTGenerator jwtGenerator;
 
 
     @Autowired
@@ -33,12 +37,14 @@ public class AuthController {
                           UserRepo userRepo,
                           RoleRepo roleRepo,
                           PasswordEncoder passwordEncoder,
-                          RegisterService registerService) {
+                          RegisterService registerService,
+                          JWTGenerator jwtGenerator) {
         this.authenticationManager = authenticationManager;
         this.userRepo = userRepo;
         this.roleRepo = roleRepo;
         this.passwordEncoder = passwordEncoder;
         this.registerService = registerService;
+        this.jwtGenerator = jwtGenerator;
 
     }
     @PostMapping("/register")
@@ -50,13 +56,14 @@ public class AuthController {
         }
     }
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO){
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginDTO loginDTO){
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                 loginDTO.getUsername(), loginDTO.getPassword())
         );
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        return ResponseEntity.ok("Login Successful");
+        String token = jwtGenerator.generateToken(authentication);
+        return new ResponseEntity<>(new AuthResponseDTO(token), HttpStatus.OK);
     }
 
 
